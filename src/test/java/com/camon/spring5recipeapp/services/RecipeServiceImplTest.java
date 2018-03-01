22f3165bc053038474c721bfc5ef34997959f1f3
camon <camon85@gmail.com>
2018-03-02 00:13:59 +0900

    circleCI

--- src/test/java/com/camon/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/camon/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -39,6 +39,6 @@ public class RecipeServiceImplTest {
         Set<Recipe> recipes = recipeService.getRecipes();
 
         assertEquals(recipes.size(), 1);
-        verify(recipeRepository, times(3)).findAll();
+        verify(recipeRepository, times(1)).findAll();
     }
 }
