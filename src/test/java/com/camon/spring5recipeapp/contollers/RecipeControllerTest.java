79327a7b7eb729fa829ad95f88affa5506923197
camon <camon85@gmail.com>
2018-03-04 03:11:25 +0900

    thymeleaf 적용. hint 주는방법

--- src/test/java/com/camon/spring5recipeapp/contollers/RecipeControllerTest.java
+++ src/test/java/com/camon/spring5recipeapp/contollers/RecipeControllerTest.java
@@ -9,12 +9,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class RecipeControllerTest {
 
@@ -24,7 +22,7 @@ public class RecipeControllerTest {
     RecipeController controller;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         controller = new RecipeController(recipeService);
     }
@@ -40,6 +38,7 @@ public class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
 }
